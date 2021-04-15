ac2ccee57ae4725645eb337de51be6aca972753d
yoshitsugu.maitani <y.fit.m0305@gmail.com>
2021-04-15 12:08:54 +0000

    tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,18 +1,17 @@
 class TasksController < ApplicationController
-    before_action :set_task, only: [:show, :edit, :update, :destroy]
+    before_action :set_task, only: [:update, :destroy]
+    before_action :require_user_logged_in
+    before_action :correct_user, except: [:index, :new, :create]
     def index
-        @tasks = Task.order(id: :desc).page(params[:page]).per(3)
+        @tasks = current_user.tasks.order(id: :desc).page(params[:page]).per(15)
     end
-    
-    def show
-    end
-    
+
     def new
-        @task = Task.new
+        @task = current_user.tasks.build
     end
     
     def create
-        @task = Task.new(task_params)
+        @task = current_user.tasks.build(task_params)
         
         if @task.save
             #taskのセーブに成功した場合、下記の文章がユーザーにお知らせとしてWeb上に表示される。
@@ -25,9 +24,6 @@ class TasksController < ApplicationController
         end
     end
 
-    def edit
-    end
-    
     def update
         if @task.update(task_params)
             flash[:success] = "タスクは正常に更新されました"
@@ -40,22 +36,27 @@ class TasksController < ApplicationController
     
     def destroy
         @task.destroy
-        
         flash[:success] =  "タスクは削除されました"
-        redirect_to tasks_url
+        redirect_back(fallback_location: root_path)
     end
     
 #privateは、それより下に示されたメソッドはアクションでなく、このクラス内でのみ使用することを明示している。
     private
-    
     def set_task
         @task = Task.find(params[:id])
     end
-    
+
     #Strong Parameter
     #パラメータを把握し、送信されたデータを精査しようとする（今回はcontentカラムだけが欲しいのでそれ以外はフィルターにかかる）
     def task_params
         #params.require(:task)で、taskモデルのフォームから得られるデータと指定。:permit(:content)で必要なカラムだけを選択
         params.require(:task).permit(:content, :status)
+    end
+    
+    def correct_user
+       @task = current_user.tasks.find_by(id: params[:id])
+        unless @task
+        redirect_to root_url
+        end
     end
 end
